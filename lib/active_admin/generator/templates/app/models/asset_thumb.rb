d6c83f5268a2376943b39c4502f35b98e0b8dc7f
Stefano Verna <stefano.verna@welaika.com>
2013-03-08 12:02:55 +0100

    Improvements in Dragonfly config

--- lib/active_admin/generator/templates/app/models/asset_thumb.rb
+++
@@ -1,7 +0,0 @@
-class AssetThumb < ActiveRecord::Base
-  attr_accessible :job, :uid
-
-  before_destroy do |thumb|
-    Dragonfly[:images].datastore.destroy(thumb.uid)
-  end
-end
--- lib/active_admin/generator/templates/config/initializers/dragonfly.rb
+++ lib/active_admin/generator/templates/config/initializers/dragonfly.rb
@@ -1,8 +1,40 @@
+require 'uri'
 require 'dragonfly'
-app = Dragonfly[:images]
+begin
+  require 'rack/cache'
+rescue LoadError => e
+  puts "Couldn't find rack-cache - make sure you have it in your Gemfile:"
+  puts "  gem 'rack-cache', :require => 'rack/cache'"
+  puts " or configure dragonfly manually instead of using 'dragonfly/rails/images'"
+  raise e
+end
 
-app.configure_with(:imagemagick)
+app = Dragonfly[:images]
 app.configure_with(:rails)
+app.configure_with(:imagemagick)
+
+if defined?(ActiveRecord::Base)
+  app.define_macro(ActiveRecord::Base, :image_accessor)
+  app.define_macro(ActiveRecord::Base, :file_accessor)
+end
+
+rack_cache_already_inserted = Rails.application.config.action_controller.perform_caching && Rails.application.config.action_dispatch.rack_cache
+
+Rails.application.middleware.insert 0, Rack::Cache, {
+  :verbose     => true,
+  :metastore   => URI.encode("file:#{Rails.root}/tmp/dragonfly/cache/meta"), # URI encoded in case of spaces
+  :entitystore => URI.encode("file:#{Rails.root}/tmp/dragonfly/cache/body")
+} unless rack_cache_already_inserted
+
+Rails.application.middleware.insert_after Rack::Cache, Dragonfly::Middleware, :images
+
+class AssetThumb < ActiveRecord::Base
+  attr_accessible :job, :uid
+  before_destroy do |thumb|
+    Dragonfly[:images].datastore.destroy(thumb.uid)
+  end
+end
+
 if ENV['S3_BUCKET']
   app.cache_duration = 3600*24*365*3
   app.configure do |c|
@@ -24,5 +56,3 @@ if ENV['S3_BUCKET']
   end
 end
 
-app.define_macro(ActiveRecord::Base, :image_accessor)
-app.define_macro(ActiveRecord::Base, :file_accessor)
